367b77c56c41bf29ec21788d4a546ad8cffb592d
Maiko-Robles <mrobless@unal.edu.co>
2025-07-16 20:12:16 -0500

    dado.py

--- dado.py
+++ dado.py
@@ -9,4 +9,12 @@ class Dado:
         if self.modo == 'real':
             return random.randint(1, 6)
         else:
-            return int(input("Ingrese el valor del dado (1-6): "))
+            while True:
+                try:
+                    valor = int(input("Ingrese el valor del dado (1-6): "))
+                    if 1 <= valor <= 6:
+                        return valor
+                    else:
+                        print("❌ El valor debe estar entre 1 y 6.")
+                except ValueError:
+                    print("❌ Entrada inválida. Debe ser un número entero.")
