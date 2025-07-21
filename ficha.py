59d03cf9d37b8f3f828499f939d76a3341b1fcfd
Maiko-Robles <mrobless@unal.edu.co>
2025-07-20 22:36:52 -0500

    ficha.py

--- ficha.py
+++ ficha.py
@@ -1,11 +1,15 @@
-# ficha.py
 class Ficha:
     def __init__(self, jugador, id_ficha):
         self.jugador = jugador
         self.id = id_ficha
         self.en_carcel = True
         self.en_llegada = False
-        self.posicion = None  # None si está en la cárcel, entero si está en el tablero
+        self.posicion = None
+        self.pasos_llegada = 0  # <--- NECESARIO para manejar llegada
+
+    @property
+    def color(self):
+        return self.jugador.color
 
     def __str__(self):
         return f"{self.jugador.color[0]}{self.id}"
