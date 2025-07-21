7745ab9dd2045b23b5693489205f6a95292fc718
Maiko-Robles <mrobless@unal.edu.co>
2025-07-20 23:38:20 -0500

    constantes.py

--- constantes.py
+++ constantes.py
@@ -1,9 +1,31 @@
 
 NUM_CASILLAS_TABLERO = 68
+NUM_CASILLAS = NUM_CASILLAS_TABLERO
+
 NUM_CASILLAS_LLEGADA = 8
+
 COLORES = ['Rojo', 'Azul', 'Verde', 'Amarillo']
+
 FICHAS_POR_JUGADOR = 4
+
 MAX_FICHAS_POR_CASILLA = 2
+
 VALOR_SALIDA = 5
+
 MOV_EXTRA_CAPTURA = 20
 MOV_EXTRA_LLEGADA = 10
+
+CASILLAS_SALIDA = {
+    "Rojo": 0,
+    "Azul": 17,
+    "Amarillo": 34,
+    "Verde": 51
+}
+
+CASILLAS_LLEGADA = {
+    "Rojo": [60, 61, 62, 63, 64, 65],
+    "Verde": [17, 18, 19, 20, 21, 22],
+    "Amarillo": [34, 35, 36, 37, 38, 39],
+    "Azul": [51, 52, 53, 54, 55, 56]
+}
+
